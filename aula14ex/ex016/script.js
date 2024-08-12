064e0881536e709c48dd40981191ffc2530a93ef
raquel <raquelvargas0905@gmail.com>
2024-08-12 20:07:51 -0300

    ex016 testando sozinha

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,9 @@
+function contar(){
+    var i = window.document.getElementById('inicio')
+    var f = window.document.querySelector('input#fim')
+    var p = window.document.querySelector('input#passo')
+
+    for (var c = i; c <= f; c++)
+    console.log(c)
+}
+
