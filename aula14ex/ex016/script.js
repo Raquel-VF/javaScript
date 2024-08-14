723c3568a49ed2b81e32fcdf3202728c38aee88d
raquel <raquelvargas0905@gmail.com>
2024-08-13 22:31:07 -0300

    ex 17 tabuada

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,9 +1,31 @@
 function contar(){
-    var i = window.document.getElementById('inicio')
-    var f = window.document.querySelector('input#fim')
-    var p = window.document.querySelector('input#passo')
+    var ini = window.document.getElementById('txti')
+    var fim = window.document.querySelector('input#txtf')
+    var passo = window.document.querySelector('input#txtp')
+    var res = document.getElementById('res')
 
-    for (var c = i; c <= f; c++)
-    console.log(c)
-}
-
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        //window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = "Impossível contar!"
+    } else {
+        res.innerHTML = 'Contando... <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            window.alert('Passo invélido! Considerando PASSO 1')
+            p = 1
+        }
+        if ( i < f) {
+            //contagem crescente
+            for (let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            } 
+        }else {
+            //contagem regressiva
+                for (let c = i; c >= f; c -= p)
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
+    }
